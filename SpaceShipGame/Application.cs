86723178d864b349421ad3d8a142df2ea430f38b
Christoffer S <christoffer.sladin@hotmail.com>
2022-11-15 15:57:05 +0100

    Add project files.

---
+++ SpaceShipGame/Application.cs
@@ -0,0 +1,102 @@
+﻿using SpaceShipGame;
+
+class Application
+{
+    public void Run()
+    {
+        var game = new MyTry();
+
+        game.Player();
+    }
+
+//    static void WriteHere(int x, int y, string s)
+//    {
+//        foreach (var line in s.Split('\n'))
+//        {
+//            Console.SetCursorPosition(x, y);
+//            Console.Write(line);
+//            y++;
+//        }
+//    }
+
+    //    public void Run()
+    //    {
+    //        var rnd = new Random();
+    //        string player = @"
+    //    ^
+    //    o
+    //  | o |
+    ///\\\o///\
+    //  </o\>";
+
+
+
+    //        int x = 0;
+    //        int y = 0;
+    //        char laser = '|';
+    //        int moves = 0;
+
+    //        int xLaser = rnd.Next(), yLaser = rnd.Next();
+    //        WriteHere(xLaser, yLaser, laser.ToString());
+    //        WriteHere(x, y, player);
+
+
+
+    //        // game loop
+    //        while (true)
+    //        {
+
+    //            Console.SetCursorPosition(0, 0);
+
+    //            if (Console.KeyAvailable)
+    //            {
+    //                switch (Console.ReadKey(true).Key)
+    //                {
+    //                    case ConsoleKey.W:
+    //                        {
+    //                            if (y > 0)
+    //                            {
+    //                                //y -= 1;
+    //                                y--; // ist //y = y - 1;
+    //                            }
+    //                            moves++;
+    //                        }
+    //                        break;
+
+    //                    case ConsoleKey.S:
+    //                        {
+    //                            if (y < - 1)
+    //                            {
+    //                                y++; // ist y = y + 1;
+    //                            }
+    //                            moves++;
+    //                        }
+    //                        break;
+
+    //                    case ConsoleKey.A:
+    //                        {
+    //                            if (x > 0)
+    //                            {
+    //                                x--;
+    //                            }
+    //                            moves++;
+    //                        }
+    //                        break;
+
+    //                    case ConsoleKey.D:
+    //                        {
+    //                            if (x < - 1)
+    //                            {
+    //                                x++;
+    //                            }
+    //                            moves++;
+    //                        }
+    //                        break;
+    //                }
+
+    //                Console.Clear();
+    //                WriteHere(x, y, player);
+    //            }
+    //        }
+    //    }
+}
---
+++ SpaceShipGame/MyTry.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceShipGame
+{
+    class MyTry
+    {
+        public int height = Console.BufferHeight;
+        public int width = Console.BufferWidth;
+
+        public void Display(int x, int y, string s)
+        {
+            while (true) 
+            {
+                if (y <= 0)
+                {
+                    y = 0;
+                    break;
+                }
+                    
+                if (x <= 0)
+                {
+                    x = 0;
+                    break;
+                }
+                break;
+            }
+
+            Console.Clear();
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(s);
+
+        }
+
+        public void Player()
+        {
+            int x = 0;
+            int y = 0;
+            Console.CursorVisible = false;
+
+            while (true)
+            {
+                string s = "|===0===|";
+
+
+                var move = Console.ReadKey(true).Key;
+
+                switch (move)
+                {
+                    case
+                        ConsoleKey.W:
+                        {
+
+                            y--;
+                            Display(x, y, s);
+
+                            break;
+                        }
+
+                    case
+                    ConsoleKey.S:
+                        {
+
+                            y++;
+
+                            Display(x, y, s);
+
+                            break;
+                        }
+
+                    case
+                    ConsoleKey.A:
+                        {
+
+                            x--;
+
+                            Display(x, y, s);
+
+                            break;
+                        }
+
+                    case
+                    ConsoleKey.D:
+                        {
+
+                            x++;
+
+                            Display(x, y, s);
+
+                            break;
+                        }
+                }
+            }
+        }
+        public void Laser()
+        {
+            var space = Console.ReadKey(true).Key;
+
+            switch (space)
+            {
+                case ConsoleKey.Spacebar:
+                    {
+
+                        break;
+                    }
+            }
+
+        }
+    }
+}
